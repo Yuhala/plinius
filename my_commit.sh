03f03f358a260481e3f2c73f6ba51747ce7957fb
Yuhala <petersonyuhala@gmail.com>
2021-11-28 12:06:34 +0100

    testing diff configs

--- my_commit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BRANCH=${1?No branch specified}
-MESS=${2?No commit message specified}
-
-git add .
-git commit --author="xhercules <xhercules95@gmail.com>" -m $MESS
-git push origin $BRANCH
